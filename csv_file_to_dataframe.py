d0030fe92b1b30d26d19512712173d10da1a67f7
emmachamberlain <61465002+emmachamberlain@users.noreply.github.com>
2020-04-04 13:17:07 +0100

    Updated so all files in the academy folder will go into the dataframe

--- csv_file_to_dataframe.py
+++ csv_file_to_dataframe.py
@@ -1,15 +1,19 @@
 import boto3
-from pprint import pprint
-import csv
 import pandas as pd
+
 
 s3_client = boto3.client('s3')
 contents = s3_client.list_objects_v2(Bucket='data8-engineering-project')['Contents']
+df_list =[]
 for x in contents:
     splitfile = x['Key'].split('/')
     if splitfile[0] == 'Academy':   ###doesnt work because all the taqlent ones are more than 1000th in the list
         data = s3_client.get_object(Bucket='data8-engineering-project',
                                     Key = x['Key'])
-        df = pd.read_csv(data['Body'])
+        df1 = pd.read_csv(data['Body'])
+        df_list.append(df1)
+
+df = pd.concat(df_list)
+
 
 print(df)
