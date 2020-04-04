aacef42b3692cfa181a7d7ee35c0acdffaf545cb
emmachamberlain <61465002+emmachamberlain@users.noreply.github.com>
2020-04-04 14:01:10 +0100

    Put into a function so the bucket and folder can be specified and also checks file is csv

--- csv_file_to_dataframe.py
+++ csv_file_to_dataframe.py
@@ -1,19 +1,21 @@
 import boto3
 import pandas as pd
 
+def dataframe(bucket, folder):
+    s3_client = boto3.client('s3')
+    contents = s3_client.list_objects_v2(Bucket=bucket, Prefix=f'{folder}/')['Contents']
+    df_list =[]
+    for x in contents:
+        splitkey = x['Key'].split('.')
+        if splitkey[-1] == 'csv':
+            data = s3_client.get_object(Bucket='data8-engineering-project',
+                                        Key = x['Key'])
+            df1 = pd.read_csv(data['Body'])
+            df_list.append(df1)
+        else:
+            pass
+    df = pd.concat(df_list)
+    return df
 
-s3_client = boto3.client('s3')
-contents = s3_client.list_objects_v2(Bucket='data8-engineering-project')['Contents']
-df_list =[]
-for x in contents:
-    splitfile = x['Key'].split('/')
-    if splitfile[0] == 'Academy':   ###doesnt work because all the taqlent ones are more than 1000th in the list
-        data = s3_client.get_object(Bucket='data8-engineering-project',
-                                    Key = x['Key'])
-        df1 = pd.read_csv(data['Body'])
-        df_list.append(df1)
 
-df = pd.concat(df_list)
-
-
-print(df)
+print(dataframe('data8-engineering-project', 'Talent'))
