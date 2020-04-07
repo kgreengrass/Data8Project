a8e740c0937fc8c3b96324d4e8fd8827b515f2bc
emmachamberlain <61465002+emmachamberlain@users.noreply.github.com>
2020-04-07 15:04:30 +0100

    updated to account for all names, however returns lowercase names

--- split_name_auto.py
+++ split_name_auto.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import re
 
-split_before_list = ['O', "O'", 'Mc', 'Van', 'Von', 'Di', 'Degli', 'Dell', 'De', 'Le', 'Du', 'St']
+split_before_list = ['o', "o'", 'mc', 'van', 'von', 'di', 'degli', 'dell', 'de', 'le', 'du', 'st', 'la', 'ten']
 
 def common_member(a, b):
     a_set = set(a)
@@ -13,6 +14,8 @@ def splitname(name):  # takes in the name column of a dataframe
     name_list=[]
     for row in name:  # goes through each row of the file being uploaded
         row = row.strip()  # strips any whitespace off beginning and end
+        row = re.sub("\s*(\W)\s*",r"\1",row)
+        row = row.lower()
         if row.count(' ') > 1:  # if there are more than 2 spaces
             # if the name contains words from this list split before them
             ## first we split by spaces and then if includes what's in the list then split before it
@@ -30,14 +33,3 @@ def splitname(name):  # takes in the name column of a dataframe
 
     namedf = pd.DataFrame(name_list)  # creates a dataframe from the name list created above
     return namedf  # returns a dataframe of first and last name
-
-
-# interview_df = pd.read_csv('C:/Users/TECH-W80.LT-RICH-W80/Desktop/cleaning dev/interview_notes1.csv')
-df = pd.DataFrame([['Casian Pavel'], ['Anna Maia Caruso'], ['ann Van der money']],
-                  columns=['name'])
-# temp_df = splitname(interview_df.name)
-# temp_df.to_csv('names_auto1.csv')
-
-print(splitname(df.name))
-#
-# print(common_member(['ann', 'van', 'der', 'money'], split_before_list))
